7170dce2fc6db57f780b8760b209c2a54b3d9ebf
Ty Crawford <tygcrawford@gmail.com>
2020-11-17 03:22:58 -0500

    login/signup fixes
    added error messages for bad login/signup and made usernames unique

--- public/scripts/auth/auth.js
+++ public/scripts/auth/auth.js
@@ -7,25 +7,44 @@ auth.onAuthStateChanged((user) => {
     }
 });
 
+async function checkUsernameUnique(username) {
+    let query = await db.collection("users").where("username", "==", username).get();
+    return query.empty;
+}
+
+function errorMessage(message) {
+    let notif = document.querySelector("#error-notif");
+    notif.innerText = message;
+    notif.classList.remove("is-hidden")
+}
+
 // user signup
 const signup_form = document.querySelector("#signup-form");
-signup_form.addEventListener("submit", (e) => {
+signup_form.addEventListener("submit", async (e) => {
     creatingAccount = true;
     e.preventDefault();
 
     const username = signup_form["signup-username"].value
     const email = signup_form["signup-email"].value
     const password = signup_form["signup-password"].value
-    
-    auth.createUserWithEmailAndPassword(email, password).then(async (creds) => {
-        const user_data = {
-            username: username,
-            correct: 0,
-            incorrect: 0
-        }
-        await db.collection("users").doc(creds.user.uid).set(user_data);
-        window.location.href = "pages/quiz.html"
-    });
+
+    let unique = await checkUsernameUnique(username);
+
+    if (unique) {
+        auth.createUserWithEmailAndPassword(email, password).then(async (creds) => {
+            const user_data = {
+                username: username,
+                correct: 0,
+                incorrect: 0
+            }
+            await db.collection("users").doc(creds.user.uid).set(user_data);
+            window.location.href = "pages/quiz.html"
+        }).catch((e) => {
+            errorMessage("This email is already associated with an account.");
+        });
+    } else {
+        errorMessage("This username is already taken.")
+    }
 });
 
 // user login
@@ -37,6 +56,8 @@ login_form.addEventListener("submit", (e) => {
     const password = login_form["login-password"].value;
 
     auth.signInWithEmailAndPassword(email, password).then((cred) => {
-        console.log("logged in")
+        // console.log("logged in")
+    }).catch((e) => {
+        errorMessage("Email or password incorrect.");
     });
 });
