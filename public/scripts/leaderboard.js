b6a7bd7c362733408891592c7371d8ddf633b48a
Ty Crawford <tygcrawford@gmail.com>
2020-11-17 02:36:14 -0500

    autocomplete search finished

--- public/scripts/leaderboard.js
+++ public/scripts/leaderboard.js
@@ -1,11 +1,11 @@
 async function main() {
     let top = await db.collection("users").orderBy("correct", "desc").limit(10).get();
-    const table = document.querySelector("#table");
+    const table = document.querySelector("#leaderboard-table");
     let i = 0;
     top.forEach(function (e) {
         let data = e.data()
         let rowTemplate = `<tr>
-            <td>${i+1}</td>
+            <td><strong>${i+1}</strong></td>
             <td>${data.username}</td>
             <td>${data.correct}</td>
             <td>${data.incorrect}</td>
@@ -15,24 +15,57 @@ async function main() {
     });
 }
 
-async function searchUsernames(string) {
-    let names = await db.collection("users").orderBy("username").startAt([string]).endAt([string + '\uf8ff']).limit(5).get()
-    names.forEach(n => {
-        console.log(n.data())
-    })
+async function getAllUsernames() {
+    let query = await db.collection("users").get()
+    let data = query.docs.map( doc => doc.data().username )
+    return data
 }
 
 async function getCurrentUserScore() {
     let uid = auth.currentUser.uid;
     let res = await db.collection("users").doc(uid).get();
-    let data = res.data();
-    document.getElementById("score").innerHTML = `<strong>Username:</strong> ${data.username}<br>
-    <strong>Correct:</strong> ${data.correct}<br>
-    <strong>Incorrect:</strong> ${data.incorrect}`;
+    return res.data();
+}
+
+async function displayCurrentUserScore() {
+    let user = await getCurrentUserScore();
+    let rowTemplate = `<tr>
+            <td>${user.username}</td>
+            <td>${user.correct}</td>
+            <td>${user.incorrect}</td>
+        </tr>`;
+    document.getElementById("currentuser-table").innerHTML = rowTemplate;
+}
+
+async function getUserWithUsername(username) {
+    let query = await db.collection("users").where("username", "==", username).get();
+    if(query.empty) return undefined;
+    return query.docs[0].data();
 }
 
 auth.onAuthStateChanged((user) => {
-  getCurrentUserScore();
+  displayCurrentUserScore();
 })
 
 main();
+
+
+$(async () => {
+    $("#search-box").on("change", async (e) => {
+        let username = e.target.value;
+        let user = await getUserWithUsername(username);
+        if (user) {
+            $("#userinfo-table-container").toggleClass("is-hidden")
+            let rowTemplate = `<tr>
+                    <td>${user.username}</td>
+                    <td>${user.correct}</td>
+                    <td>${user.incorrect}</td>
+                </tr>`;
+            $("#userinfo-table").html(rowTemplate);
+        }
+    });
+
+    $("#search-box").autocomplete({
+        source: await getAllUsernames()
+    });
+})
